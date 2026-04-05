34548a8abbc862fbec6b172e8ea2666a1c0ba7fa
Fokoda799 <feyt2003@gmail.com>
2026-04-05 20:59:27 +0100

    fix: Add SSL configuration for Redis connection pool in production settings

--- frontstore/settings/prod.py
+++ frontstore/settings/prod.py
@@ -23,6 +23,7 @@ CACHES = {
         "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None},
         },
     }
 }
