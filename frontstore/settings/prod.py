c023e7e933b2ccc671cb90d44df31d194e383442
Fokoda799 <feyt2003@gmail.com>
2026-04-05 22:53:16 +0100

    fix: Update SSL configuration for Celery broker and result backend

--- frontstore/settings/prod.py
+++ frontstore/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 
 import dj_database_url
 
@@ -16,9 +17,9 @@ REDIS_URL = os.environ["REDIS_URL"]
 CELERY_BROKER_URL = "rediss://default:nVqgWiHe40XLgUITWQzC4DuBw6u4quHj@redis-16942.c44.us-east-1-2.ec2.cloud.redislabs.com:16942"  # noqa: E501
 CELERY_RESULT_BACKEND = "rediss://default:nVqgWiHe40XLgUITWQzC4DuBw6u4quHj@redis-16942.c44.us-east-1-2.ec2.cloud.redislabs.com:16942"  # noqa: E501
 # Required for Redis Cloud SSL
-CELERY_BROKER_USE_SSL = {"ssl_cert_reqs": None}
+CELERY_BROKER_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
 
-CELERY_RESULT_BACKEND_USE_SSL = {"ssl_cert_reqs": None}
+CELERY_RESULT_BACKEND_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
 
 CACHES = {
     "default": {
