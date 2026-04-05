a135bcc565fde288aa8fa05d65879a44844fe3d3
Fokoda799 <feyt2003@gmail.com>
2026-04-05 19:23:41 +0100

    fix: Add ordering option for Product model by title

--- store/models.py
+++ store/models.py
@@ -56,6 +56,9 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ["title"]
+
 
 class ProductImage(models.Model):
     product = models.ForeignKey(
