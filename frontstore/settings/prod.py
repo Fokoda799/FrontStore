8a93381eb377e839e3a597dc4fb10c766805743c
Fokoda799 <feyt2003@gmail.com>
2026-04-05 16:25:37 +0100

    fix: Correct ALLOWED_HOSTS assignment to ensure it is a list

--- frontstore/settings/prod.py
+++ frontstore/settings/prod.py
@@ -8,7 +8,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"]
+ALLOWED_HOSTS = [os.environ["ALLOWED_HOSTS"]]
 
 DATABASES = {"default": dj_database_url.config()}
 
