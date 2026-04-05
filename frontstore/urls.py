50d356d12452330a00c6c0c721f2373c913715f8
Fokoda799 <feyt2003@gmail.com>
2026-04-05 19:15:46 +0100

    fix: Update URL configuration for improved routing and admin settings

--- frontstore/urls.py
+++ frontstore/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -47,6 +46,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
     urlpatterns = (
         urlpatterns
         + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
